623a5cb15d5924da7da297fbf7d204ffb9ca977b
DVela-Code <ddvela@up.edu.ph>
2024-12-07 21:52:25 +0800

    Add files via upload

--- 3D printing - Copy/js/main.js
+++ 3D printing - Copy/js/main.js
@@ -6,27 +6,27 @@ function togglemenu(){
     navMobile.classList.toggle('active');
 }
 
-// Change Background Videos//
+// Change Background Videos
 function changeVideo(name){
-    const bgVideoList = document.querySelectorAll('.bg-video')
-    const videoContainer = document.querySelectorAll('.video-container')
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const videoContainer = document.querySelectorAll('.video-container');
 
-    bgVideoList.forEach(video =>{
-        video.classList.remove('active')
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
         if(video.classList.contains(name)){
-            video.classList.add('active')
+            video.classList.add('active');
         }
     });
 
-    videoContainer.forEach(video =>{
-        video.classList.remove('active')
+    videoContainer.forEach(video => {
+        video.classList.remove('active');
         if(video.classList.contains(name)){
-            video.classList.add('active')
+            video.classList.add('active');
         }
     });
 }
 
-// Autoscrolling for the navigation - for smooth scrolling - remove this if you want a more snappy transition aka just jumping to that section
+// Smooth scrolling for navigation
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
@@ -37,25 +37,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 // Navigation Dropdown
-// document.addEventListener('DOMContentLoaded', function () {
-//     document.querySelector('.dropbtn').addEventListener('click', function () {
-//         var dropdownContent = this.nextElementSibling;
-//         dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
-//     });
-// });
 document.addEventListener('DOMContentLoaded', function () {
     const dropbtn = document.querySelector('.dropbtn');
     const dropdownContent = document.querySelector('.dropdown-content');
 
-    // Toggle dropdown on button click
     dropbtn.addEventListener('click', function (event) {
-        // Prevent the click event from propagating to the document
         event.stopPropagation();
-        // Toggle the display of the dropdown content
         dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
     });
 
-    // Close the dropdown if the user clicks outside of it - not really necessary, but still looks good
     document.addEventListener('click', function(event) {
         if (!dropbtn.contains(event.target) && !dropdownContent.contains(event.target)) {
             dropdownContent.style.display = 'none';
