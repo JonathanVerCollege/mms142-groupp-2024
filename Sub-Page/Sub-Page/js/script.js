42229f5546de16ed6a8db695a3847f0a7baf5c0a
DVela-Code <ddvela@up.edu.ph>
2024-12-08 04:51:36 +0800

    Update script.js

--- Sub-Page/Sub-Page/js/script.js
+++ Sub-Page/Sub-Page/js/script.js
@@ -85,3 +85,20 @@ document.querySelectorAll('.nav-mobile li a').forEach(item => {
         body.classList.remove('no-scroll');
     });
 });
+
+// Navigation Dropdown
+document.addEventListener('DOMContentLoaded', function () {
+    const dropbtn = document.querySelector('.dropbtn');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    dropbtn.addEventListener('click', function (event) {
+        event.stopPropagation();
+        dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!dropbtn.contains(event.target) && !dropdownContent.contains(event.target)) {
+            dropdownContent.style.display = 'none';
+        }
+    });
+});
