a843b7a8946ae6005da4c71e30eeb7799d09637c
DVela-Code <ddvela@up.edu.ph>
2024-12-08 00:35:19 +0800

    Update script.js

--- Sub-Page/Sub-Page/js/script.js
+++ Sub-Page/Sub-Page/js/script.js
@@ -21,3 +21,13 @@ var swiper = new Swiper(".swiper", {
     clickable: true
   }
 });
+
+//smooth scrolling for navigation//
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
